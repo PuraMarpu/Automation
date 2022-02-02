6e2aae7256619b0b150893572b06f501b0457d9b
Obnoxius_Guy <72298201+PuraMarpu@users.noreply.github.com>
2022-02-02 17:48:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import cv2 
-import os
 import dropbox
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv('TOKEN')
 
 def AccessCamera():
     camera = cv2.VideoCapture(0)
@@ -15,13 +9,13 @@ def AccessCamera():
         t = cv2.imwrite('something.png', img)
         print(t)
         Bool = False
-    
+
     camera.release()
 
 AccessCamera()
 
 def main():
-    Dropb = dropbox.Dropbox('TOKEN')
+    Dropb = dropbox.Dropbox('sl.BBS7VB5Q17zs9UVtik6i-yqA2ppabPmwp8eTsW0KHfnoyMAiBr5OUXf8IE2ZDwJ55McE9PtK9ywpWFsRDOVK7FgE49FMGGyPie4AQ2WTUI_EZQcntfqct6ryRXfUDObhm7t6NDNJwGgM')
 
     with open('something.png', 'rb') as file:
         Dropb.files_upload(file.read(), '/Automation--')
